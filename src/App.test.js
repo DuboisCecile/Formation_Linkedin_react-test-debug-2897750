d18c1e46e41de410a1f3c847c2b73455540964b1
Cécile Dubois <duboiscecilepro@gmail.com>
2023-03-17 20:39:11 +0100

    Ajouté des commentaires d'explication

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// On teste si le composant App est bien rendu
 test('should render App', () => {
     const { container } = render(<App />);
     expect(container).toBeInTheDocument();
 });
 
+// On teste si l'élément avec le texte 'La Todo Liste' est bien présent et visible
 test('should render heading', () => {
     render(<App />);
     const heading = screen.getByText('La Todo Liste');
     expect(heading).toBeVisible();
 });
 
+// On teste si l'élément avec le rôle (la valeur sémantique, c'est à dire ce qui est renvoyé par le DOM à l'agent utilisateur) heading est bien présent et visible
 test('should render heading list todo', () => {
     render(<App />);
     const heading = screen.getByRole('heading');
