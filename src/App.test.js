58661f11a60f044019bb4ba9b3913c96db24ef63
Cécile Dubois <duboiscecilepro@gmail.com>
2023-03-17 20:34:44 +0100

    Fin exo 02-03 avec corrections par rapport à la formation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Learn React Testing Library/i);
-    expect(linkElement).toBeInTheDocument();
+test('should render App', () => {
+    const { container } = render(<App />);
+    expect(container).toBeInTheDocument();
 });
+
+test('should render heading', () => {
+    render(<App />);
+    const heading = screen.getByText('La Todo Liste');
+    expect(heading).toBeVisible();
+});
+
+test('should render heading list todo', () => {
+    render(<App />);
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeVisible();
+});
+
+// import { render } from "@testing-library/react"
+// import App from './App'
+
+// test('should render App', () => {
+//     const { container } = render(<App />)
+//     expect(container).toBeInTheDocument()
+// })
+
+// test('should render heading', () => {
+//     const { getByText } = render(<App />)
+//     const heading = getByText('La Todo Liste');
+//     expect(heading).toBeVisible()
+// })
+
+// test('should render heading list todo', () => {
+//     const { getByRole } = render(<App />)
+//     const heading = getByRole('heading');
+//     expect(heading).toBeVisible()
+// })
